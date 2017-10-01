8450b787f5ab1ff04a937e756bf98948f0554e0f
Daniel <mail@danielluedecke.de>
2017-10-01 15:45:14 +0200

    fix issues in get_dv_labels and get_model_labels

--- R/get_model_labels.R
+++ R/get_model_labels.R
@@ -68,7 +68,7 @@ get_term_labels <- function(models, mark.cat = FALSE, case = NULL) {
   # get model terms and model frame
 
   m <- purrr::map(models, ~ dplyr::slice(broom::tidy(.x, effects = "fixed"), -1))
-  mf <- purrr::map(models, ~ dplyr::select(stats::model.frame(.x), -1))
+  mf <- purrr::map(models, ~ dplyr::select(get_model_frame(.x), -1))
 
 
   # get all variable labels for predictors
@@ -157,7 +157,7 @@ get_dv_labels <- function(models, case = NULL) {
 
   # get intercept vectors
 
-  intercepts <- purrr::map(models, ~ dplyr::pull(stats::model.frame(.x), var = 1))
+  intercepts <- purrr::map(models, ~ dplyr::pull(get_model_frame(.x), var = 1))
   intercepts.names <- purrr::map(models, ~ deparse(stats::formula(.x)[[2L]]))
 
 
@@ -182,4 +182,18 @@ get_dv_labels <- function(models, case = NULL) {
   if (length(lbs) > 1) lbs <- "Dependent variable"
 
   convert_case(lbs, case)
+}
+
+
+
+#' @importFrom nlme getResponse getData getCovariateFormula
+#' @importFrom stats model.frame
+get_model_frame <- function(x) {
+  if (inherits(x, c("lme", "gls"))) {
+    y <- nlme::getResponse(x)
+    mf <- cbind(y, nlme::getData(x)[, all.vars(nlme::getCovariateFormula(x))])
+    colnames(mf)[1] <- get_label(y, def.value = "Response")
+    as.data.frame(mf)
+  } else
+    stats::model.frame(x)
 }
