99b3f6262379c81a3e41293bf98b83158485173e
Daniel <mail@danielluedecke.de>
2017-10-04 21:51:01 +0200

    revise vignette

--- R/helpfunctions.R
+++ R/helpfunctions.R
@@ -166,15 +166,10 @@ isempty <- function(x, first.only = TRUE) {
 }
 
 
+
+#' @importFrom snakecase to_any_case
 convert_case <- function(lab, case) {
   if (!is.null(case) && !is.null(lab)) {
-
-    # check if package available
-    if (!requireNamespace("snakecase", quietly = TRUE)) {
-      message("Package `snakecase` required for case-conversion.")
-      return(lab)
-    }
-
     snakecase::to_any_case(
       lab,
       case = case,
