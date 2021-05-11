a1de7411dd297d0a1398fa33e5e16edbba1f2693
Daniel <mail@danielluedecke.de>
2021-05-11 09:14:16 +0200

    fix

--- R/as_labelled.R
+++ R/as_labelled.R
@@ -94,21 +94,23 @@ as_labelled_helper <- function(x, add.labels, add.class, skip.strings, tag.na) {
     if (!requireNamespace("haven", quietly = TRUE)) {
       stop("Package 'haven' required for this function. Please install it.")
     }
-    labels <- attr(x, "labels", exact = TRUE)
     xna <- get_na(x, as.tag = TRUE)
-    new_tags <- unname(gsub("NA\\((.*)\\)", "\\1", xna))
-    names(new_tags) <- new_tags
-    # convert to numeric, if character
-    numeric_na <- which(is.na(suppressWarnings(as.numeric(new_tags))))
-    if (any(numeric_na)) {
-      names(new_tags)[numeric_na] <- match(new_tags[numeric_na], letters) * -1
+    if (!isempty(xna)) {
+      labels <- attr(x, "labels", exact = TRUE)
+      new_tags <- unname(gsub("NA\\((.*)\\)", "\\1", xna))
+      names(new_tags) <- new_tags
+      # convert to numeric, if character
+      numeric_na <- which(is.na(suppressWarnings(as.numeric(new_tags))))
+      if (any(numeric_na)) {
+        names(new_tags)[numeric_na] <- match(new_tags[numeric_na], letters) * -1
+      }
+      tagged_missing <- haven::na_tag(x)
+      for (i in 1:length(xna)) {
+        x[which(tagged_missing == new_tags[i])] <- as.numeric(names(new_tags[i]))
+      }
+      labels[is.na(labels)] <- stats::setNames(attr(x, "na.values"), names(labels[is.na(labels)]))
+      attr(x, "labels") <- labels
     }
-    tagged_missing <- haven::na_tag(x)
-    for (i in 1:length(xna)) {
-      x[which(tagged_missing == new_tags[i])] <- as.numeric(names(new_tags[i]))
-    }
-    labels[is.na(labels)] <- stats::setNames(attr(x, "na.values"), names(labels[is.na(labels)]))
-    attr(x, "labels") <- labels
   }
 
   # is type of labels same as type of vector? typically, character
